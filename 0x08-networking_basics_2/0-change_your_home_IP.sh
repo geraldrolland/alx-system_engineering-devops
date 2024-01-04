b5870ea1e079ee29c2c73b8f21920b02d31b089a
geraldrolland <geraldrolland123@gmail.com>
2024-01-04 14:16:17 -0800

    my task 0

--- 0x08-networking_basics_2/0-change_your_home_IP.sh
+++ 0x08-networking_basics_2/0-change_your_home_IP.sh
@@ -1,22 +1,6 @@
 #!/usr/bin/env bash
 # configures an Ubuntu server with the below requirements.
-cd /
-touch etc/hosts.new
-file_path="etc/hosts"
-if [[ -e "$file_path" ]]
-then
-    while read -r line
-    do
-       content=$(echo "$line" | grep -o "localhost")
-       if [[ "$content" == "localhost" ]]
-       then
-           echo "127.0.0.2	localhost" >> etc/hosts.new
-	   continue
-       fi
-       echo "$line" >> etc/hosts.new
-    done < "$file_path"
-fi
-echo "8.8.8.8		facebook.com" >> etc/hosts.new
-cat etc/hosts.new > etc/hosts
-
-       
+cp /etc/hosts ~/hosts.new
+echo "127.0.0.2		localhost" > ~/hosts.new
+echo "8.8.8.8		facebook.com" >> ~/hosts.new
+cp -f ~/hosts.new /etc/hosts       
