491b80f2b103a7e5b06a102fbd5dab9a1e2f989c
geraldrolland <geraldrolland123@gmail.com>
2024-06-30 07:08:47 -0700

    puppet

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,10 +1,10 @@
 # Ensure that pip3 is installed and up to date
-package { 'python3-pip':
-  ensure => 'present',
+package { 'python':
+  ensure => 'install',
 }
 
 package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
-  require  => Package['python3-pip'],
+  require  => Package['python'],
 }
