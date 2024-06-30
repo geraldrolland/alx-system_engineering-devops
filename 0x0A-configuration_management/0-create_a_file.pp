cd3f7ad745bd69b29fe5f96cd3f4bbd1827f83f2
geraldrolland <geraldrolland123@gmail.com>
2024-06-30 06:12:50 -0700

    puppet

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,5 +1,5 @@
-file { "/temp/school":
-         ensure => "present,
+file { '/temp/school':
+         ensure => "present",
          mode => 0744,
          owner => "www-data",
          group => "www-data",
