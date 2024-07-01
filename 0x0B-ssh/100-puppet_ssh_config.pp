1e1962df0a06192fbe2d2227593b8304a2e2eab7
geraldrolland <geraldrolland123@gmail.com>
2024-07-01 06:59:09 -0700

    ssh

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,5 +1,8 @@
 #setup ssh client configuration
+file { '/root/.ssh':
+  ensure => 'directory'
+}
 file { '/root/.ssh/config':
-  ensure => 'present',
+  ensure  => 'present',
   content => template('/etc/ssh/config.erb')
 }
